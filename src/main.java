6f8a75ad686a509a1c74f781493dd1daf3aee891
Bradley Erickson <bbwe24@gmail.com>
2020-04-18 16:08:56 -0500

    wrote initial code, fixed main

--- src/main.java
+++ src/main.java
@@ -1,13 +1,13 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-import app.TFTPClientNet;
+import app.TFTPclient;
 
 public class main {
 
     public static void main(String[] args) throws IOException {
 		
-		TFTPClientNet tftpClient = new TFTPClientNet();
+		TFTPclient client;
         Scanner scan = new Scanner(System.in);
         boolean end = false;
 
@@ -18,27 +18,37 @@ public class main {
 			System.out.println("2 - Write Request");
 			System.out.println("3 - Exit");
 			String option = scan.nextLine();
-			String fileName = null;
+			String input = null;
 			switch(option) {
 				case "1":
+					System.out.print("Enter server address: ");
+					input = scan.nextLine();
+					System.out.println("Connecting to " + input);
+					client = new TFTPclient(input);
 					System.out.print("Enter File Name Requested: ");
-					fileName = scan.nextLine();
-					tftpClient.get(fileName);
+					input = scan.nextLine();
+					client.getFile(input);
+					System.out.println("Terminating connection with server.");
 				break;
 				case "2":
+					System.out.print("Enter server address: ");
+					input = scan.nextLine();
+					System.out.println("Connecting to " + input);
+					client = new TFTPclient(input);
 					System.out.print("Enter File Name Requested: ");
-					fileName = scan.nextLine();
-					tftpClient.put(fileName);
+					input = scan.nextLine();
+					client.putFile(input);
+					System.out.println("Terminating connection with server.");
 				break;
 				case "3":
 					end = true;
-					System.out.println("TFTP Client Exiting...");
 				break;
 				default:
-					System.out.println("Command Not Recognized");
+					System.out.println("Command not recognized");
 			}
-        }
-        
+		}
+
+		System.out.println("Exiting program.");
         scan.close();
 	}
 
